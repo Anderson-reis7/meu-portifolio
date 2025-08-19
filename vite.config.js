8e7a971e2955b43042e8a7717079a9dca653a8d7
Anderson reis <anderson.200810@gmail.com>
2025-08-19 14:59:46 -0300

    fix: config base para github pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/meu-portifolio/', // 👈 igualzinho ao nome do repositório no GitHub
-  plugins: [react(), tailwindcss],
+  base: "/meu-portifolio/", // 👈 nome exato do seu repositório no GitHub
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
